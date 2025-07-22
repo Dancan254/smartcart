b8ca271d3dac04aa5c5bec3e657dda4890e5b6a4
Dancan254 <dancanian25@gmail.com>
2025-07-22 20:15:14 +0300

    create the customer repository interface

---
+++ src/main/java/com/javaguy/smartcart/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.javaguy.smartcart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
